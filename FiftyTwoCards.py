35d40c06da37992705179cc0c55557b9588637d3
mnf0212053 <94829566+mnf0212053@users.noreply.github.com>
2022-01-07 15:57:17 +0700

    add initialization function

--- FiftyTwoCards.py
+++ FiftyTwoCards.py
@@ -1,3 +1,18 @@
 import sqlite3
 
 con = sqlite3.connect("card52.db")
+
+def create_cards(conn):
+    sql_delete_table = ''' DROP TABLE IF EXISTS cards; '''
+    sql_create = """ CREATE TABLE IF NOT EXISTS cards(
+        id integer PRIMARY KEY,
+        rank text,
+        suit text,
+        possession text
+    ); """
+
+    cur = conn.cursor()
+    cur.execute(sql_delete_table)
+    cur.execute(sql_create)
+
+create_cards(con)
