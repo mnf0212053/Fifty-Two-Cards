d995e810db760e979fcee279932d8be82d0d487e
mnf0212053 <94829566+mnf0212053@users.noreply.github.com>
2022-01-07 16:17:34 +0700

    update card initialization (inserting cards into table)

--- FiftyTwoCards.py
+++ FiftyTwoCards.py
@@ -14,5 +14,20 @@ def create_cards(conn):
     cur = conn.cursor()
     cur.execute(sql_delete_table)
     cur.execute(sql_create)
+    
+    sql_insert = ''' INSERT INTO cards(rank, suit, possession) VALUES (?, ?, ?) '''
 
-create_cards(con)
+    suits = ('Heart', 'Diamond', 'Club', 'Spade')
+    royal_ranks = ('Ace', 'King', 'Queen', 'Jack')
+
+    for royal in royal_ranks:
+        for suit in suits:
+            sql_cards_data = (royal, suit, 'None')
+            cur.execute(sql_insert, sql_cards_data)
+
+    for lower_ranks in range(2, 11):
+        for suit in suits:
+            sql_cards_data = (str(12 - lower_ranks), suit, 'None')
+            cur.execute(sql_insert, sql_cards_data)
+    conn.commit()
+
